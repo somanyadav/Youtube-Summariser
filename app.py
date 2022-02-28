65f2a049ec76a5e903ba6465d7ed39c169cd2c75
Soman Yadav <52402870+somanyadav@users.noreply.github.com>
2022-02-28 11:23:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,54 @@
+# Running Streamlit
 import streamlit as st
 
-st.write("Hello World")
+# Extracting Transcript from YouTube
+import pafy
+from youtube_transcript_api import YouTubeTranscriptApi
+from urllib.parse import urlparse
+from textwrap import dedent
+
+
+
+# Input Video Link
+url = st.sidebar.text_input('Video URL', 'https://www.youtube.com/watch?v=T-JVpKku5SI')
+
+# Display Video and Title
+video = pafy.new(url)
+value = video.title
+st.info("### Title : " + value)
+st.video(url)
+
+# If Summarize button is clicked
+if st.sidebar.button('Summarize'):
+    st.success(dedent("""### \U0001F4D6 Summary
+> Success!
+    """))
+
+    # Generate Transcript by slicing YouTube link to id 
+    url_data = urlparse(url)
+    id = url_data.query[2::]
+    
+    def generate_transcript(id):
+            transcript = YouTubeTranscriptApi.get_transcript(id)
+            script = ""
+
+            for text in transcript:
+                    t = text["text"]
+                    if t != '[Music]':
+                            script += t + " "
+                    
+            return script, len(script.split())
+    transcript, no_of_words = generate_transcript(id)
+    
+
+    # Priting Transcript in "JUSTIFY" alignment
+    html_str = f"""
+<style>
+p.a {{
+text-align: justify;
+}}
+</style>
+<p class="a">{transcript}</p>
+"""
+    st.markdown(html_str, unsafe_allow_html=True)
+    

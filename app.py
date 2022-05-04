fe2b81ee07284d9f6e297261ba8eb4b2f177f4a6
Soman Yadav <somanyadavsalwan@gmail.com>
2022-05-04 15:01:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -366,7 +366,7 @@ if sumtype == 'Extractive':
 
              threshold = _find_average_score(sentence_scores)
 
-             summary = _generate_summary(sentences, sentence_scores, 1.0 * threshold)
+             summary = _generate_summary(sentences, sentence_scores, 0.6 * threshold)
             
              summ = summary
               
